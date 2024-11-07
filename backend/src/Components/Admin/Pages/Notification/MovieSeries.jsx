ceb7272cb14f0021c8105a039662fc271aae3245
Vivek kumar <vivekkumar787067@gmail.com>
2024-11-08 01:52:10 +0530

    clean code

--- backend/src/Components/Admin/Pages/Notification/MovieSeries.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const MovieSeries = () => {
-  return (
-    <div>MovieSeries</div>
-  )
-}
-
-export default MovieSeries
--- backend/src/Components/Admin/Pages/Notification/Webview.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Webview = () => {
-  return (
-    <div>Webview</div>
-  )
-}
-
-export default Webview
