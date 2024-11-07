47b5281eeeff2b35344de0361a3fd720067be091
Vivek kumar <vivekkumar787067@gmail.com>
2024-11-08 01:35:08 +0530

    frontend and backend

---
+++ backend/model/AllTvSeries/AllTvSeriesModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const TvSeriesSchema = new mongoose.Schema({
+  tmdbId: { type: String, unique: true },
+  title: { type: String, required: true },
+  slug: { type: String, required: true, unique: true },
+  description: { type: String },
+  actors: { type: String },
+  directors: { type: String },
+  writers: { type: String },
+  imdbRating: { type: String },
+  releaseDate: { type: Date },
+  countries: { type: String, default: 'India' },
+  genres: { type: String },
+  runtime: { type: String },
+  freePaid: { type: String, enum: ['Free', 'Paid'], default: 'Paid' },
+  trailerUrl: { type: String },
+  videoQuality: { type: String, default: '4K' },
+  thumbnail: { type: String }, // Store the base64 image URL
+  poster: { type: String }, // Store the base64 image URL
+  sendNewsletter: { type: Boolean, default: false },
+  sendPushNotification: { type: Boolean, default: false },
+  publish: { type: Boolean, default: false },
+  enableDownload: { type: Boolean, default: false },
+}, { timestamps: true });
+
+const TvSeries = mongoose.model('TvSeries', TvSeriesSchema);
+
+module.exports = TvSeries;
